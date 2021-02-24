d7cc5a9af05872636a1244936afe3b48a641a88f
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2021-02-24 23:01:37 +0100

    Setup basic structure for changing the language

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {language: 'english'};
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+        <div className="ui container">
+          <div>
+            Select a language:
+            <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+            <i className="flag nl" onClick={ () => this.onLanguageChange('dutch')}/>
+          </div>
+
+          {this.state.language}
+        </div>
+    );
   }
+
+  onLanguageChange = (newLanguage) => {
+    this.setState({language: newLanguage});
+  };
 }
 
 export default App;

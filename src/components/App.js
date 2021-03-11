3448c686b35bda2cb94a062575861452f8707fec
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-03-11 17:37:04 +0100

    Use the Context.Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends React.Component {
   state = {language: 'english'};
@@ -10,10 +11,12 @@ class App extends React.Component {
           <div>
             Select a language:
             <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
-            <i className="flag nl" onClick={ () => this.onLanguageChange('dutch')}/>
+            <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
           </div>
 
-          <UserCreate/>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate/>
+          </LanguageContext.Provider>
         </div>
     );
   }
